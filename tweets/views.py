e52389eaa34f621e41a397575fd65fe08e84d47d
William Chung <118134309+WLL1am@users.noreply.github.com>
2024-07-24 17:51:32 -0700

    home and tweet url error handling

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+
+from.models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    data = {
+        "id": tweet_id,
+        "content": obj.content,
+        # "image_path": object.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    
+    return JsonResponse(data, status=status)
+        
